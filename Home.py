5f6989316598f137f628957fda315904896486f1
fxpeterbs31 <107398327+fxpeterbs31@users.noreply.github.com>
2023-06-24 14:29:31 +0700

    initial_commit

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown('# BRIDGE MOTION SIMULATION BASED ON NONSTANDARD FINITE DIFFERENCE METHOD IN PYTHON')
+
+st.write('\n\n')
+st.write('**By:**')
+st.write('PETER BUNTARAN SUHARYO')
+st.write('2201750961')
